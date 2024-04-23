957dda3bd59e8f9d680173c78e11720774d6ab1a
herrera <juancamiloherrerabohorquez@gmail.com>
2024-04-23 08:26:41 -0500

    añadi tabla stock paara ingresar cada uno de los objetos a reservar

--- app/src/main/java/com/example/intentovacio/Activity/bdd.java
+++ app/src/main/java/com/example/intentovacio/Activity/bdd.java
@@ -7,7 +7,8 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
-class bdd extends SQLiteOpenHelper{
+
+class bdd  extends SQLiteOpenHelper{
 
     private Context context ;
     private static final  String DATABESE_NAME ="Basedd_ul.db";
@@ -19,6 +20,14 @@ class bdd extends SQLiteOpenHelper{
 
     private static final  String  COLUMNA_CLAVE ="clave";
     private static final  String  COLUMNA_NUMERO ="numero";
+
+    private static final String  Nombre_tabla_s ="stock";
+    private static final String  Columna_Id_s ="Id_stock";
+
+    private static final  String  COLUMNA_NOMBRE ="Nobj";
+
+
+
 
 
 
@@ -35,26 +44,47 @@ class bdd extends SQLiteOpenHelper{
                 COLUMNA_NUMERO + " INT, " +
                 COLUMNA_CLAVE + " TEXT)";
         db.execSQL(query);
+
+        String stock = "CREATE TABLE " + Nombre_tabla_s +
+                "(" +  Columna_Id_s + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMNA_NOMBRE  + " TEXT)";
+        db.execSQL(stock);
+
+
     }
 
-     @Override
-     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + Nombre_tabla);
         onCreate(db);
-     }
-     void añadir_usuario (String correo , String clave ){
+    }
+    void añadir_usuario (String correo , String clave ){
         SQLiteDatabase db = this.getWritableDatabase();
-         ContentValues cv = new ContentValues();
+        ContentValues cv = new ContentValues();
 
-         cv.put(COLUMNA_CORREO, correo);
-         cv.put(COLUMNA_CLAVE, clave);
-         long result = db.insert(Nombre_tabla,null,cv);
-         if (result == -1){
-             Toast.makeText(context, "fallo :(", Toast.LENGTH_SHORT).show();
-         }
-         else {
-             Toast.makeText(context, "insert realizado", Toast.LENGTH_SHORT).show();
-         }
+        cv.put(COLUMNA_CORREO, correo);
+        cv.put(COLUMNA_CLAVE, clave);
+        long result = db.insert(Nombre_tabla,null,cv);
+        if (result == -1){
+            Toast.makeText(context, "fallo :(", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(context, "insert realizado", Toast.LENGTH_SHORT).show();
+        }
 
-     }
- }
+    }
+    void añadir_Stock (String obj ){
+        SQLiteDatabase db2 = this.getWritableDatabase();
+        ContentValues cv2 = new ContentValues();
+
+        cv2.put(COLUMNA_NOMBRE, obj);
+        long result = db2.insert(Nombre_tabla_s,null,cv2);
+        if (result == -1){
+            Toast.makeText(context, "fallo :(", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(context, "insert realizado", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
