4eac957c4d82dceda57d14948aa0966c47443b9d
Luis Miguel Mican Leal <luismiguelmc02@hotmail.com>
2024-05-15 20:29:00 -0500

    Base de datos

--- app/src/main/java/com/example/intentovacio/Helper/ManagmentReservas.java
+++ app/src/main/java/com/example/intentovacio/Helper/ManagmentReservas.java
@@ -3,60 +3,61 @@ package com.example.intentovacio.Helper;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.example.intentovacio.Activity.bdd;
 import com.example.intentovacio.Domain.ListDomain;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+
 public class ManagmentReservas {
     private Context context;
     private TinyDB tinyDB;
+    private bdd myDB;
 
     public ManagmentReservas(Context context) {
         this.context = context;
-        this.tinyDB= new TinyDB(context);
+        this.tinyDB = new TinyDB(context);
+        this.myDB = new bdd(context);
     }
 
-    public void  insertFood(ListDomain item){
+    public void insertFood(ListDomain item) {
         ArrayList<ListDomain> listPop = getListReservas();
-        boolean existAlready=false;
-        Random n= new Random();
+        boolean existAlready = false;
+        Random n = new Random();
+
         /*
-        for (int i=0; i<listPop.size();i++){
-            if(listPop.get(i).getTitle().equals(item.getTitle())){
-                existAlready=true;
-                n=i;
+        for (int i = 0; i < listPop.size(); i++) {
+            if (listPop.get(i).getTitle().equals(item.getTitle())) {
+                existAlready = true;
+                n = i;
                 break;
             }
         }
         */
 
-
-        if(existAlready){
-            listPop.get(n.nextInt(4)+1).setCode(item.getCode());
-        }else{
+        if (existAlready) {
+            listPop.get(n.nextInt(4) + 1).setCode(item.getCode());
+        } else {
             listPop.add(item);
         }
         tinyDB.putListObject("Reservas", listPop);
+        myDB.anadirReserva(item.getTitle(), item.getCode()); // Añadir reserva a la base de datos
         Toast.makeText(context, "Reserva realizada", Toast.LENGTH_SHORT).show();
     }
-    public ArrayList<ListDomain> getListReservas(){
-        return tinyDB.getListObject("Reservas");
 
+    public ArrayList<ListDomain> getListReservas() {
+        return tinyDB.getListObject("Reservas");
     }
 
-    public void minusNumber(ArrayList<ListDomain>listItem, int position, ChangeCodeItemsListener changeCodeItemsListener){
-        if (listItem.get(position).getCode()==1){
+    public void minusNumber(ArrayList<ListDomain> listItem, int position, ChangeCodeItemsListener changeCodeItemsListener) {
+        if (listItem.get(position).getCode() == 1) {
             listItem.remove(position);
-        }else{
-            listItem.get(position).setCode(listItem.get(position).getCode()-1);
+        } else {
+            listItem.get(position).setCode(listItem.get(position).getCode() - 1);
         }
         tinyDB.putListObject("Reservas", listItem);
         changeCodeItemsListener.change();
     }
-
-
-
-
 }
