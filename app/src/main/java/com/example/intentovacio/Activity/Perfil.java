f5fcaccdfe13facc85c76bd6f9a3e223ec4d2765
Version1 <juandiegorodriguezgalindo982@gmail.com>
2024-05-21 20:31:34 -0500

    Version Inicial 11 *Cambio total al apartado de perfil, ahora es util :P

--- app/src/main/java/com/example/intentovacio/Activity/Perfil.java
+++ app/src/main/java/com/example/intentovacio/Activity/Perfil.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.intentovacio.Helper.ManagmentReservas;
 import com.example.intentovacio.R;
@@ -15,24 +16,12 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.w3c.dom.Text;
 
-public class Perfil extends Activity {
+public class Perfil extends AppCompatActivity {
 
-    TextView correo,celular,codigo,carrera;
-    private FirebaseFirestore mDatabase;
-    @Override
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
-        mDatabase = FirebaseFirestore.getInstance();
-        correo = findViewById(R.id.correo);
-        celular = findViewById(R.id.celular);
-        codigo = findViewById(R.id.codigo);
-        carrera = findViewById(R.id.carrera);
-        mDatabase.collection("Usuario").document(correo.getText().toString()).get().addOnSuccessListener(resultado->{
-            String correo1 = resultado.getString("correo");
-            Double celular  = resultado.getDouble("celular");
-            correo.setText("El numero de celular es: " + celular);
-        });
 
     }
 }
